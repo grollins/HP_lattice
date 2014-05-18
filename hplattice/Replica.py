9402d23f586c196a71a13563f7c9fca366680726
Geoff Rollins <grollins@gmail.com>
2014-05-17 17:49:32 -0700

    Fix spacing in Replica module.

--- hplattice/Replica.py
+++ hplattice/Replica.py
@@ -1,26 +1,18 @@
-#! /usr/bin/python
-#
-# Replica.py    
+from string import zfill
 
 from .Chain import Chain
 from .Monty import Monty
 
-import random
-import string
-import math
-import sys
-import os
 
 class Replica:
     """A container object, to hold the Chain() and Monty() objects"""
 
-    def __init__(self,config,repnum):
+    def __init__(self, config, repnum):
         """Initialize the Replica() object."""
-	
-	temp = config.REPLICATEMPS[repnum]
-	self.repnum = repnum
-	self.repname = 'rep' + string.zfill(str(repnum),2)
-	self.repdir = config.DATADIR + self.repname
-	self.chain = Chain(config)
-	self.mc = Monty(config,temp,self.chain)    
-    
+        temp = config.REPLICATEMPS[repnum]
+        self.repnum = repnum
+        self.repname = 'rep' + zfill( str(repnum), 2 )
+        self.repdir = config.DATADIR + self.repname
+        self.chain = Chain(config)
+        self.mc = Monty(config, temp, self.chain)    
+
