aa41cd752a3343b92f4b14ba8ac77c263d9b1749
Geoff Rollins <grollins@gmail.com>
2014-05-27 16:05:49 -0700

    Implement script to profile the performance of enumerating states.

---
+++ benchmarks/new_enumeration/profile_enumerate.py
@@ -0,0 +1,33 @@
+import cProfile
+import pstats
+from os.path import join
+
+from hplattice.Config import Config
+from hplattice.Enumerator import Enumerator
+
+
+HP_STRING_SET = (12, 'HPHPPHPHPHPH')
+
+
+def load_configuration(hp_string, initial_vec):
+    configfile = 'enumerate.conf'
+    config = Config( filename=configfile )
+    config.HPSTRING = hp_string
+    config.INITIALVEC = initial_vec
+    config.RESTRAINED_STATE = []
+    return config
+
+def main():
+    N, hp_string = HP_STRING_SET
+    initial_vec = [0] * (N - 1)
+    config = load_configuration(hp_string, initial_vec)
+    en = Enumerator(config, verbose=False)
+    en.enumerate_states()
+
+if __name__ == '__main__':
+    filename = './profile_stats.stats'
+    cProfile.run('main()', filename)
+    stats = pstats.Stats(filename)
+    stats.strip_dirs()
+    stats.sort_stats('cumulative')
+    stats.print_stats(20)
