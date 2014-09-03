e71fd55b39984b189faa6c7f471d6ac37a944d92
Geoff Rollins <grollins@gmail.com>
2014-09-03 14:41:56 -0700

    Add docs for LatticeFactory.

--- hplattice/__init__.py
+++ hplattice/__init__.py
@@ -6,7 +6,18 @@ from .Replica import Replica
 
 
 class LatticeFactory(object):
-    """docstring for LatticeFactory"""
+    """
+    *LatticeFactory* objects orchestrate the creation of other objects
+    in the HP model, namely chains, replicas, monte carlo samplers,
+    trajectories, and configurations. 
+
+    :param callable chain_cls: optional, chain class
+    :param callable replica_cls: optional, replica class
+    :param callable monty_cls: optional, monte carlo sampler class
+    :param callable trajectory_cls: optional, trajectory class
+    :param callable conf_cls: optional, HP configuration class
+
+    """
     def __init__(self, chain_cls=Chain, replica_cls=Replica, monty_cls=Monty,
                  trajectory_cls=Trajectory, conf_cls=Config):
         self.chain_cls = chain_cls
@@ -16,16 +27,31 @@ class LatticeFactory(object):
         self.conf_cls = conf_cls
 
     def make_chain(self, *args, **kwargs):
+        """
+        Make a chain object.
+        """
         return self.chain_cls(*args, **kwargs)
 
     def make_replica(self, *args, **kwargs):
+        """
+        Make a replica object.
+        """
         return self.replica_cls(*args, **kwargs)
 
     def make_monty(self, *args, **kwargs):
+        """
+        Make a monte carlo sampler object.
+        """
         return self.monty_cls(*args, **kwargs)
 
     def make_trajectory(self, *args, **kwargs):
+        """
+        Make a trajectory object.
+        """
         return self.trajectory_cls(*args, **kwargs)
 
     def make_configuration(self, *args, **kwargs):
+        """
+        Make a configuration object.
+        """
         return self.conf_cls(*args, **kwargs)
