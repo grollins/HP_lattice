ba5e7a8f59c84d145c40ebd655d2d787c054cb0f
Geoff Rollins <grollins@gmail.com>
2014-09-04 11:23:02 -0700

    Mock out cython components in docs.

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import sys
 import os
 
 import mock
-MOCK_MODULES = ['numpy', 'scipy']
+MOCK_MODULES = ['hplattice.util', 'numpy', 'scipy']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
